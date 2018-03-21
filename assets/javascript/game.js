7af7cafb2d5b392e1d1a3a99ddb5811759fddf5b
Michael Myers <myersmyer@gmail.com>
2018-03-21 15:38:05 -0400

    initial number gen is up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,3 @@
-//PSEUDOCODE
-//PAGE WILL LOAD
-//COMPUTER SCORE DISPLAY RANDOM NUMBER BETWEEN 19-120
-//EACH PIC/BUTTON WILL HOLD A RANDOM UNDISPLAYED NUMBER BETWEEN 1-12
 //USER CLICKS BUTTONS TO ADD TO THEIR SCORE
 //USER SCORE UPDATES WITH NUMBERS FROM PIC/BUTTONS AND DISPLAYS TOTAL
 //IF USER SCORE EQUALS COMPUTER SCORE EXACTLY USER WIN COUNTER INCREASE BY 1 AND GAME RESETS
@@ -9,10 +5,39 @@
 //GAME RESET FUNCTION WILL GENERATE NEW COMPUTER SCORE, NEW RANDOM PIC NUMBERS, AND CLEAR USER SCORE
 
 
+// LOAD
 $(document).ready(function() {
-    $("li").on("click", function() {
-      alert("I've been clicked!");
-      console.log("I'm working");
-    });
 
-  });
+// CREATE GLOBAL VARIABLES
+var userScoreCounter = 0;
+// Generate computer random between 19-120 and assign to a variable
+var compNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
+// Generate random numbers between 1 and 12 for each button image
+var numRock1 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var numRock2 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var numRock3 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var numRock4 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+
+
+// TEST AND DEBUG
+console.log(numRock1);
+console.log(numRock2);
+console.log(numRock3);
+console.log(numRock4);
+console.log("This is the comp number: " + compNumber);
+
+// MAIN GAMEPLAY FUNCTIONs
+
+// Placing computer number over section
+$('#cpuScore').text(compNumber);
+    
+
+//TESTING TO MAKE SURE MY BUTTON CLICKS WORK
+$(".images").on("click", function() {
+alert("I'm an image and I've been clicked!");
+console.log("I'm working");
+});
+
+//CALLING THE FUNCTION TO RUN THE GAME****************************************************************    
+
+});
