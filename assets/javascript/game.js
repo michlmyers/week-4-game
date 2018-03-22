ec3a6e6ee32105015059f77f59a771b9e9df566a
Michael Myers <myersmyer@gmail.com>
2018-03-21 22:46:12 -0400

    some changes game is closer now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,13 @@
 
 // CREATE VARIABLE COUNTER
 var userScoreCounter = 0;
+var winCount = 0;
+var lossCount = 0;
 
 // LOAD
 $(document).ready(function() {
 
-// Generate computer random between 19-120 and assign to a variable
-var compNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
+  var compNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
 
 // Generate random numbers between 1 and 12 for each button image
 var numRock1 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
@@ -15,6 +16,11 @@ var numRock2 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 var numRock3 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 var numRock4 =  Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
+ //THIS SHOULD BE MY RANDOM NUMBER FUNCTIONS AT GAME START AND CLEAR 
+// Generate computer random between 19-120 and assign to a variable
+
+//this line starts the "New Game" function
+function newGame() {
 // TEST AND DEBUG
 console.log(numRock1);
 console.log(numRock2);
@@ -24,52 +30,122 @@ console.log("This is the comp number: " + compNumber);
 
 // Placing computer number over section
 $('#cpuScore').text(compNumber);
+$('#userScore').text(userScoreCounter);
+}//this line ends the "New Game" function
 
+// This calls the new game function. 
+newGame();
+
+// ----------------------------------------------------------------------------------------------------------
 //USER CLICKS IMAGE BUTTONS TO ADD TO THEIR SCORE COUNTER
 //USER SCORE UPDATES WITH NUMBERS FROM PIC/BUTTONS AND DISPLAYS TOTAL
+
+// ------------------------------------------------------------------------------
 
 // First image click + add
 $("#rock1").on("click", function() {
 userScoreCounter += numRock1;  
 // Placing user count over section
 $('#userScore').text(userScoreCounter);
+
+// trying the if else here
+if (userScoreCounter > compNumber) {
+  alert("Bummer, you've been cursed!");
+  lossCount++;
+  $('#playerLoss').text("LOSE: " + lossCount);
+  console.log("Player lost");
+} else if (userScoreCounter == compNumber){
+  alert("Yeah dawg, you won!");
+  winCount++;
+  $('#playerWin').text("WIN: " + winCount);
+  console.log("Player won");
+}
+
 // TEST AND DEBUG
-console.log("Crystal image 1 clicked x: " + userScoreCounter)});
+console.log("Crystal image 1 clicked x: " + userScoreCounter)
+});
+
+// ------------------------------------------------------------------------------
 
 // Second image click + add
 $("#rock2").on("click", function() {
 userScoreCounter += numRock2;  
 // Placing user count over section
 $('#userScore').text(userScoreCounter);
+
+// trying the if else here
+if (userScoreCounter > compNumber) {
+  alert("Bummer, you've been cursed!");
+  lossCount++;
+  $('#playerLoss').text("LOSE: " + lossCount);
+  console.log("Player lost");
+} else if (userScoreCounter == compNumber){
+  alert("Yeah dawg, you won!");
+  winCount++;
+  $('#playerWin').text("WIN: " + winCount);
+  console.log("Player won");
+}
+
 // TEST AND DEBUG
-console.log("Crystal image 2 clicked x: " + userScoreCounter)});
+console.log("Crystal image 2 clicked x: " + userScoreCounter)
+});
+
+// ------------------------------------------------------------------------------
 
 // Third image click + add
 $("#rock3").on("click", function() {
 userScoreCounter += numRock3;  
 // Placing user count over section
 $('#userScore').text(userScoreCounter);
+
+// trying the if else here
+if (userScoreCounter > compNumber) {
+  alert("Bummer, you've been cursed!");
+  lossCount++;
+  $('#playerLoss').text("LOSE: " + lossCount);
+  console.log("Player lost");
+} else if (userScoreCounter == compNumber){
+  alert("Yeah dawg, you won!");
+  winCount++;
+  $('#playerWin').text("WIN: " + winCount);
+  console.log("Player won");
+}
+
 // TEST AND DEBUG
-console.log("Crystal image 3 clicked x: " + userScoreCounter)});
+console.log("Crystal image 3 clicked x: " + userScoreCounter)
+});
+
+// ------------------------------------------------------------------------------
 
 // Fourth image click + add
 $("#rock4").on("click", function() {
 userScoreCounter += numRock4;  
 // Placing user count over section
 $('#userScore').text(userScoreCounter);
-// TEST AND DEBUG
-console.log("Crystal image 4 clicked x: " + userScoreCounter)});
 
-//IF USER SCORE EQUALS COMPUTER SCORE EXACTLY USER WIN COUNTER INCREASE BY 1 AND GAME RESETS
-//IF USER SCORE GOES ABOVE COMPUTER SCORE USER LOSS COUNTER INCREASES BY 1 AND GAME RESETS
-$("images").on("click", function() {
-if (userScoreCounter === compNumber) {
-  alert("Bummer, you lost!");
+// trying the if else here
+if (userScoreCounter > compNumber) {
+  alert("Bummer, you've been cursed!");
+  lossCount++;
+  $('#playerLoss').text("LOSE: " + lossCount);
   console.log("Player lost");
 } else if (userScoreCounter == compNumber){
   alert("Yeah dawg, you won!");
+  winCount++;
+  $('#playerWin').text("WIN: " + winCount);
   console.log("Player won");
 }
 
-})
+// TEST AND DEBUG
+console.log("Crystal image 4 clicked x: " + userScoreCounter)
+});
+
+// ----------------------------------------------------------------------------------------------------------
+
+//IF USER SCORE EQUALS COMPUTER SCORE EXACTLY USER WIN COUNTER INCREASE BY 1 AND GAME RESETS
+//IF USER SCORE GOES ABOVE COMPUTER SCORE USER LOSS COUNTER INCREASES BY 1 AND GAME RESETS
+
+// *************NEXT STEPS MIKE - CREATE WIN / LOSS DIVS*********************** 
+// *************CREATE GAME CLEAR / RESET FUNCTIONS WHEN***********************
+
 });
